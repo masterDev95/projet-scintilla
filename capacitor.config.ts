e8516d6a9e7766f976c9154f252a1318361212ce
masterDev95 <masdevelopper@gmail.com>
2022-06-11 00:27:15 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projet-tinder-esiee',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
